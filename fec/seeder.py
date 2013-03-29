09fa28b1bf26a358edc23d615d5d41fc0f1d6e05
Jay Boice <jboice@gmail.com>
2013-03-29 13:22:11 -0400

    Undo changes to seeder

--- fec/seeder.py
+++ fec/seeder.py
@@ -18,10 +18,10 @@ class Seeder(object):
 
     def download_fec_file(self):
         print "Downloading individual contributions file from the FEC"
-        filepath = os.path.join(self.path, 'indiv04.zip')
+        filepath = os.path.join(self.path, 'indiv14.zip')
         fh = open(filepath, 'wb')
         with open(filepath, 'wb') as fh:
-            subprocess.call(["curl", "-s", "ftp://ftp.fec.gov/FEC/2004/indiv04.zip"], stdout=fh)
+            subprocess.call(["curl", "-s", "ftp://ftp.fec.gov/FEC/2014/indiv14.zip"], stdout=fh)
         return filepath
 
     def unzip_fec_file(self, filepath):
@@ -31,13 +31,12 @@ class Seeder(object):
     def head_fec_file(self, lines=30000):
         print "Extracting first %s rows of FEC file" % lines
         filepath = os.path.join(self.path, 'itcont2.txt')
-        os.system("grep '|NY|' data/itcont.txt > data/itcont2.txt")
-        #with open(filepath, 'w') as fh:
-        #    subprocess.call(["grep", "'|NY|'", os.path.join(self.path, 'itcont.txt')], stdout=fh)
+        with open(filepath, 'w') as fh:
+            subprocess.call(["head", "-%s" % lines, os.path.join(self.path, 'itcont.txt')], stdout=fh)
         return filepath
 
     def load_fec_data(self, filepath):
         print "Loading FEC data"
         self.db.execute("linker", """
-            LOAD DATA INFILE %s INTO TABLE individual_contributions_2004_ny FIELDS TERMINATED BY '|' (committee_id,amendment,report_type,pgi,image_num,transaction_type,entity_type,contributor_name,city,state,zipcode,employer,occupation,transaction_date,amount,other_id,transaction_id,filing_number,memo_code,memo_text,sub_id)
+            LOAD DATA INFILE %s INTO TABLE individual_contributions_2014 FIELDS TERMINATED BY '|' (committee_id,amendment,report_type,pgi,image_num,transaction_type,entity_type,contributor_name,city,state,zipcode,employer,occupation,transaction_date,amount,other_id,transaction_id,filing_number,memo_code,memo_text,sub_id)
         """, args=[filepath,])
